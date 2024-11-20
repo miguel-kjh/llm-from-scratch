47038f8d7c82802e9501ed6d3bf880f215c9a56e
miguel-kjh <miguelangel.kjh@gmail.com>
2024-11-20 11:29:37 +0000

    add simple tokenizer with regex

--- data_prepare/tokenizer.py
+++ data_prepare/tokenizer.py
@@ -1,4 +1,36 @@
 import os
+import re
 
-FOLDER = "/home/miguel/data/01-raw/pretrain"
-FILE_PATH = os.path.join(FOLDER, "the-verdict.txt")
+class Tokenizer:
+
+    def encode(self):
+        pass
+
+class SimpleTokenizer(Tokenizer):
+
+    def __init__(self, text):
+        self.text = text
+        self.regular_expression = r'([,.:;?_!"()\']|--|\s)'
+
+    def encode(self):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', self.text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        return preprocessed
+
+
+
+# main
+if __name__ == '__main__':
+    FOLDER = "/home/miguel/data/01-raw/pretrain"
+    FILE_PATH = os.path.join(FOLDER, "the-verdict.txt")
+
+    with open(FILE_PATH, "r") as f:
+        text = f.read()
+
+    print("Total characters:", len(text))
+    print(text[:99])
+
+    tokenizer = SimpleTokenizer(text)
+    tokens = tokenizer.encode()
+    print("Total tokens:", len(tokens))
+    print(tokens[:10])
